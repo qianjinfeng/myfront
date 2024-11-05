76a24daf341436f2956878f1aee4c062b78df958
Jin Feng Qian <jin_feng.qian@siemens-healthineers.com>
2024-11-05 17:00:33 +0800

    app router

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+export default function Page() {
+    return <h1>Hello, Dashboard Page!</h1>
+  }
---
+++ app/esearch/page.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import React from "react";
+import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
+import { SearchDriver } from "@elastic/search-ui";
+import {
+  ErrorBoundary,
+  Facet,
+  SearchProvider,
+  SearchBox,
+  Results,
+  PagingInfo,
+  ResultsPerPage,
+  Paging,
+  Sorting,
+  WithSearch
+} from "@elastic/react-search-ui";
+import { Layout } from "@elastic/react-search-ui-views";
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+
+const connector = new ElasticsearchAPIConnector({
+  host: "http://localhost:9200",
+  index: "dicom",
+  apiKey: "SHlxdzRwSUJITEdvSlBXeUFzZFA6aFkzYkRWTDVSMDY0cmdKU2U4am5mUQ=="
+});
+
+const config = {
+  searchQuery: {
+    search_fields: {
+      StudyDescription: {}
+    },
+    result_fields: {
+      // PatientName.Alphabetic:{
+      //   raw: {}
+      // },
+      StudyDescription:{
+        raw: {}
+      },
+      StudyInstanceUID: {
+        snippet: {}
+      }
+    },
+    disjunctiveFacets: ["PatientSex"],
+    facets: {
+      "PatientSex": {type: "value"}
+    }
+  },
+  autocompleteQuery: {
+    results: {
+      resultsPerPage: 5,
+      search_fields: {
+        "PatientName.Alphabetic": {
+          weight: 3
+        }
+      },
+      result_fields: {
+        PatientName_Alphabetic: {
+          snippet: {
+            size: 100,
+            fallback: true
+          }
+        },
+        StudyInstanceUID: {
+          raw: {}
+        }
+      }
+    },
+    suggestions: {
+      types: {
+        results: { fields: ["PatientName_Alphabetic"] }
+      },
+      size: 4
+    }
+  },
+  apiConnector: connector,
+  alwaysSearchOnInitialLoad: true
+};
+
+const driver = new SearchDriver(config);
+
+driver.subscribeToStateChanges((state) =>
+  console.log(`Received ${state.totalResults} results for your search!`)
+);
+
+export default function Home() {
+  return (
+    <SearchProvider config={config}>
+    <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+      {({ wasSearched }) => {
+        return (
+          <div className="App">
+            <ErrorBoundary>
+              <Layout
+                header={
+                  <SearchBox
+                    autocompleteMinimumCharacters={3}
+                    autocompleteResults={{
+                      linkTarget: "_blank",
+                      sectionTitle: "Results",
+                      titleField: "PatientName",
+                      urlField: "SOPInstanceUID",
+                      shouldTrackClickThrough: true
+                    }}
+                    autocompleteSuggestions={true}
+                    debounceLength={0}
+                  />
+                }
+                sideContent={
+                  <div>
+                    {wasSearched && <Sorting label={"Sort by"} sortOptions={[]} />}
+                    <Facet key={"1"} field={"PatientSex"} label={"Sex"} />
+                  </div>
+                }
+                bodyContent={<Results shouldTrackClickThrough={true} />}
+                bodyHeader={
+                  <React.Fragment>
+                    {wasSearched && <PagingInfo />}
+                    {wasSearched && <ResultsPerPage />}
+                  </React.Fragment>
+                }
+                bodyFooter={<Paging />}
+              />
+            </ErrorBoundary>
+          </div>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
+  );
+}
